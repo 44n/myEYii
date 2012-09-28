998e54c25c4df6670815ed18a9a1a22e826bb6fe
Alexandr <tanger_wf@list.ru>
2012-09-28 20:37:33 +0300

    правка заголовков

--- protected/views/section/view.php
+++ protected/views/section/view.php
@@ -1,39 +1,39 @@
 <?php
 $this->breadcrumbs=array(
-	'Категории'=>array('index'),
-	$model->meta_title,
+    'Категории'=>array('index'),
+    $model->meta_title,
 );
 
 $this->menu=array(
-	array('label'=>'Список категорий', 'url'=>array('index')),
-	array('label'=>'Создать категорию', 'url'=>array('create')),
-	array('label'=>'Обновить гатегорию', 'url'=>array('update', 'id'=>$model->id)),
+    array('label'=>'Список категорий', 'url'=>array('index')),
+    array('label'=>'Создать категорию', 'url'=>array('create')),
+    array('label'=>'Обновить гатегорию', 'url'=>array('update', 'id'=>$model->id)),
     array('label'=>'Добавить потомка', 'url'=>array('create', 'parent'=>$model->id)),
     array('label'=>'Добавить Товар', 'url'=>array('/item/create', 'id'=>$model->id)),
-	array('label'=>'Удалить категорию', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Вы уверены ,что хотите удалить категорию?')),
-	array('label'=>'Администрировать', 'url'=>array('admin')),
+    array('label'=>'Удалить категорию', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Вы уверены ,что хотите удалить категорию?')),
+    array('label'=>'Администрировать', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Обзор категории<?php echo $model->id; ?></h1>
+<h1>Обзор категории  <?php echo $model->meta_title; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'id',
-		'parent_id',
-		'short_description',
-		'full_description',
-		'meta_title',
-		'meta_keywords',
-		'meta_description',
-		'price',
-	),
+    'data'=>$model,
+    'attributes'=>array(
+        'id',
+        'parent_id',
+        'short_description',
+        'full_description',
+        'meta_title',
+        'meta_keywords',
+        'meta_description',
+        'price',
+    ),
 )); ?>
 </br>
 </br>
 </br>
-<h1>Потомки <?php echo $model->meta_title; ?></h1>
+<h1>Потомки категории <?php echo $model->meta_title; ?></h1>
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
     'id'=>'section-model-grid',
@@ -56,7 +56,7 @@ $this->menu=array(
     ),
 )); ?>
 
-<h1> Товары <?php echo $model->meta_title; ?></h1>
+<h1> Товары категории <?php echo $model->meta_title; ?></h1>
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
     'id'=>'sectionItems',
